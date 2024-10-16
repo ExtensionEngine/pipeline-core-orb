b4b5c8716ce09a1975284bc84aad190005f36e04
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-16 14:51:54 +0200

    fix: uniform `v16.20` as the minimum supported Node.js version
    Resolves an inconsistency between documentation, tests, and version check
    for minimum supported Node.js version and seals it to `v16.20`.

--- src/scripts/check-node-version.sh
+++ src/scripts/check-node-version.sh
@@ -7,7 +7,7 @@ if [[ "$NODE_VERSION" =~ $NODE_VERSION_REGEX ]]; then
   MAJOR="${BASH_REMATCH[1]}"
   MINOR="${BASH_REMATCH[2]}"
 
-  if [[ "$MAJOR" -lt 16 || ("$MAJOR" -eq 16 && "$MINOR" -lt 17) ]]; then
+  if [[ "$MAJOR" -lt 16 || ("$MAJOR" -eq 16 && "$MINOR" -lt 20) ]]; then
     echo "At least Node.js v16.20 is required!"
     exit 1
   fi
