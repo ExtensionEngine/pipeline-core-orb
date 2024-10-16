7a3fa6aba91e0d08f06265bf9c964653d5a14ff7
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-16 15:56:38 +0200

    refactor: standardize env variable handling
    Standardizes environment variable handling by,
    - moving all definitions to the top of a file
    - using curly braces
    - prefixing all variables exported from commands
    - evaluating parameters that default to an env variable
    - using PWD env to get the working directory

--- src/scripts/check-node-version.sh
+++ src/scripts/check-node-version.sh
@@ -2,12 +2,14 @@
 
 NODE_VERSION_REGEX="v([0-9]+).([0-9]+).([0-9]+)"
 NODE_VERSION=$(node -v)
+MAJOR=""
+MINOR=""
 
-if [[ "$NODE_VERSION" =~ $NODE_VERSION_REGEX ]]; then
+if [[ "${NODE_VERSION}" =~ ${NODE_VERSION_REGEX} ]]; then
   MAJOR="${BASH_REMATCH[1]}"
   MINOR="${BASH_REMATCH[2]}"
 
-  if [[ "$MAJOR" -lt 16 || ("$MAJOR" -eq 16 && "$MINOR" -lt 20) ]]; then
+  if [[ "${MAJOR}" -lt 16 || ("${MAJOR}" -eq 16 && "${MINOR}" -lt 20) ]]; then
     echo "At least Node.js v16.20 is required!"
     exit 1
   fi
--- src/scripts/check-pkg-json.sh
+++ src/scripts/check-pkg-json.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -f "package.json" ]; then
-  echo "File package.json not found inside current directory: $(pwd)"
+  echo "File package.json not found inside current directory: ${PWD}"
   echo "Content of current directory:"
   ls -al
   exit 1
