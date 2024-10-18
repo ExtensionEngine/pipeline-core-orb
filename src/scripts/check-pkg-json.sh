9d8dfc0dfb58912fdbd0876725e6632eaae7e56f
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-18 18:48:46 +0200

    refactor: update verbosity of commands (#25)
    Increase verbosity by adding more logs to scripts, enable debugging mode
    for eval commands.

--- src/scripts/check-pkg-json.sh
+++ src/scripts/check-pkg-json.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -f "package.json" ]; then
-  echo "File package.json not found inside current directory: ${PWD}"
-  echo "Content of current directory:"
+  echo "File package.json not found inside working directory: ${PWD}"
+  echo "Content of working directory:"
   ls -al
   exit 1
 fi
--- src/scripts/run-script.sh
+++ src/scripts/run-script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "Running custom script from package.json at ${PWD}"
+
+set -x
 eval "${CURRENT_PKG_MANAGER}" run "${PARAM_STR_SCRIPT}"
+set +x
