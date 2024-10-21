9a95eb40b1cfc19f37d62ff55ad772698545a4fd
droguljic <1875821+droguljic@users.noreply.github.com>
2024-10-21 16:51:00 +0200

    style: format `sh` files
    Add empty lines between commands for better readability.

--- src/scripts/install-dependencies.sh
+++ src/scripts/install-dependencies.sh
@@ -2,13 +2,16 @@
 
 if [[ -n "${PARAM_STR_INSTALL_COMMAND}" ]]; then
   echo "Running custom install command"
+
   set -x
   eval "${PARAM_STR_INSTALL_COMMAND}"
   set +x
 elif [[ "${CURRENT_PKG_MANAGER}" == "npm" ]]; then
   echo "Running npm clean install"
+
   npm ci
 elif [[ "${CURRENT_PKG_MANAGER}" == "pnpm" ]]; then
   echo "Running pnpm install with frozen lockfile"
+
   pnpm i --frozen-lockfile
 fi
