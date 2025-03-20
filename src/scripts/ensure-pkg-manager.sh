438ae86ebed33ab5157f65dde566d54dab36ddba
droguljic <1875821+droguljic@users.noreply.github.com>
2025-03-20 13:48:23 +0100

    feat: uniform package manager default version logic
    Both npm and pnpm now default to the detected version, when the
    version is omitted. In a case where pnpm is not detected, the latest
    version will be installed.
    This change also brings few minor improvements:
    - Will not try to reinstall requested version if already installed
    - Increase log points for easier debugging.

--- src/scripts/ensure-pkg-manager.sh
+++ src/scripts/ensure-pkg-manager.sh
@@ -6,6 +6,12 @@ NAME="${PARAM_STR_REF}"
 VERSION=""
 SUDO=""
 
+if [[ ${EUID} -ne 0 ]]; then
+  echo "Using sudo privileges to finish the process"
+
+  SUDO="sudo"
+fi
+
 check_installation () {
   local installed_version
   local required_version
@@ -31,6 +37,25 @@ check_installation () {
   fi
 }
 
+change_pnpm_store_dir_and_exit () {
+  local current_store_dir
+  local target_store_dir
+
+  current_store_dir=$(pnpm store path)
+  target_store_dir="${HOME}/.pnpm_store"
+
+  if [[ "${current_store_dir}" != "${target_store_dir}" ]]; then
+    echo "Changing the pnpm store directory"
+
+    set -x
+    pnpm config set store-dir "${target_store_dir}"
+    ${SUDO} rm -rf "${current_store_dir}"
+    set +x
+  fi
+
+  exit 0
+}
+
 if [[ "${PARAM_STR_REF}" =~ ${PKG_MANAGER_WITH_VERSION_REGEX} ]]; then
   NAME="${BASH_REMATCH[1]}"
   VERSION="${BASH_REMATCH[2]}"
@@ -39,34 +64,56 @@ fi
 echo "export CURRENT_PKG_MANAGER='${NAME}'" >> "${BASH_ENV}"
 echo "Starting to ensure ${NAME} is set for usage"
 
-if [[ ${EUID} -ne 0 ]]; then
-  echo "Using sudo privileges to finish the process"
-
-  SUDO="sudo"
-fi
-
+cd ~ || echo "Cannot navigate to home, possible version mismatch"
+  
 if [[ "${NAME}" == "npm" ]]; then
+  echo "Detected npm $(npm --version)"
+
   if [[ -n "${VERSION}" ]]; then
-    ${SUDO} npm i -g npm@"${VERSION}"
-    check_installation "${NAME}" "${VERSION}"
-  else
-    echo "Detected npm version: $(npm --version)"
+    if npm --version | grep "${VERSION}" > /dev/null 2>&1; then
+      echo "Requested version of npm is already installed"
+    else
+      echo "Requested version of npm not found, updating detected version"
+    
+      ${SUDO} npm i -g npm@"${VERSION}"
+      check_installation "${NAME}" "${VERSION}"
+    fi
+
+    exit 0
   fi
+
+  echo "Using detected version of npm"
 
   exit 0
 fi
 
 if [[ "${NAME}" == "pnpm" ]]; then
   if command -v pnpm > /dev/null 2>&1; then
-    echo "Found pnpm installation, removing it"
+    echo "Detected pnpm $(pnpm --version)"
+    
+    if [[ -z "${VERSION}" ]]; then
+      echo "Using detected version of pnpm"
+
+      change_pnpm_store_dir_and_exit
+    elif pnpm --version | grep "${VERSION}" > /dev/null 2>&1; then
+      echo "Requested vesion of pnpm is already installed"
+
+      change_pnpm_store_dir_and_exit
+    fi
+
+    echo "Requested version of pnpm not found, removing detected version"
 
     ${SUDO} rm -rf "$(pnpm store path)" > /dev/null 2>&1
     ${SUDO} rm -rf "${PNPM_HOME}" > /dev/null 2>&1
     ${SUDO} npm rm -g pnpm > /dev/null 2>&1
+  else
+    echo "Did not detect pnpm, proceeding with installation"
   fi
 
   if [[ -z "${VERSION}" ]]; then
     VERSION=$(npm view pnpm version)
+
+    echo "Version not explicitly requested, opting for ${VERSION}"
   fi
 
   ${SUDO} npm i -g pnpm@"${VERSION}"
