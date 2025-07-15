f04715f48f2397d807f1d7979351064d2a757d27
droguljic <1875821+droguljic@users.noreply.github.com>
2025-07-15 14:57:15 +0200

    fix: support running script with versioned package manager
    Passing package manager with version or tag will not produce error
    since value is processed before usage.

--- src/scripts/run-script.sh
+++ src/scripts/run-script.sh
@@ -2,6 +2,17 @@
 
 echo "Running custom script from package.json at ${PWD}"
 
+PKG_MANAGER=$(circleci env subst "${PARAM_STR_PKG_MANAGER}")
+PKG_MANAGER_REGEX="^(npm|pnpm)(@.+)?$"
+
+if [[ "${PKG_MANAGER}" =~ ${PKG_MANAGER_REGEX} ]]; then
+  PKG_MANAGER="${BASH_REMATCH[1]}"
+else
+  echo "Cannot run script with unsupported package manager '${PKG_MANAGER}'"
+
+  exit 1
+fi
+
 set -x
-eval "${PARAM_STR_PKG_MANAGER}" run "${PARAM_STR_SCRIPT}"
+eval "${PKG_MANAGER}" run "${PARAM_STR_SCRIPT}"
 set +x
